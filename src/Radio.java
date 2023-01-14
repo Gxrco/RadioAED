e832ed67091a28b82d4bf797c4a16375bba4fddb
Sebastian <thiagohugo2014@gmail.com>
2023-01-14 12:00:40 -0600

    Se implemento la interfaz IRadio con sus metodos

---
+++ src/Radio.java
@@ -0,0 +1,85 @@
+public class Radio implements IRadio{
+
+    @Override
+    public void on() {
+
+    }
+
+    @Override
+    public void off() {
+
+    }
+
+    /***
+     * Este metodo nos indica si la radio esta encendida o apagada
+     * @return true si la radio esta encendida y false cuando la radio este apagada
+     */
+    @Override
+    public boolean isOn() {
+        return false;
+    }
+
+    /***
+     * Este metodo nos ayuda a establecer la frecuencia, recibe un parametro llamado freq que puede "AM" o "FM"
+     * @param freq La frecuencia la cual puede ser AM o FM, de lo contrario error.
+     */
+    @Override
+    public void setFrequence(String freq) throws Exception {
+
+    }
+
+    @Override
+    public String getFrequence() {
+        return null;
+    }
+
+    @Override
+    public void Forward() {
+
+    }
+
+    @Override
+    public void Backward() {
+
+    }
+
+    @Override
+    public double getFMActualStation() {
+        return 0;
+    }
+
+    @Override
+    public int getAMActualStation() {
+        return 0;
+    }
+
+    @Override
+    public void setFMActualStation(double actualStation) {
+
+    }
+
+    @Override
+    public void setAMActualStation(int actualStation) {
+
+    }
+
+    @Override
+    public void saveFMStation(double actualStation, int slot) {
+
+    }
+
+    @Override
+    public void saveAMStation(int actualStation, int slot) {
+
+    }
+
+    @Override
+    public double getFMSlot(int slot) {
+        return 0;
+    }
+
+    @Override
+    public int getAMSlot(int slot) {
+        return 0;
+    }
+}
